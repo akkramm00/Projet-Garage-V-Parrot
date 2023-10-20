2d865b7da23ca313d7ce536b7ebadc0a7363431f
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-10-20 18:36:25 +0200

    add faker data in database with fakerBundle

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,25 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Generator;
 use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
         $puissanceOptions = [100, 150, 200, 250];
@@ -17,7 +30,7 @@ class AppFixtures extends Fixture
         for ($p = 1; $p <= 50; $p++) {
             $products = new Products();
             $products->setMarque($marqueOptions[array_rand($marqueOptions)])
-                ->setModel('Model ')
+                ->setModel($this->faker->word())
                 ->setPrix(mt_rand('15000', '40000'))
                 ->setBoite((rand(0, 1) ? 'mecanique' : 'Automatique'))
                 ->setEnergie($energieOptions[array_rand($energieOptions)])
