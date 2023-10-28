204dccbc12193c05f1c5eb99e40ecec9e5d4674b
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-10-28 19:36:13 +0200

    add editPassword

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\UserType;
+use App\Form\UserPasswordType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -14,7 +15,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'user.index', methods: ['GET'])]
     /**
      * This controller display all users
      *
@@ -23,6 +23,7 @@ class UserController extends AbstractController
      * @param Request $request
      * @return Response
      */
+    #[Route('/user', name: 'user.index', methods: ['GET'])]
     public function index(
         UserRepository $repository,
         PaginatorInterface $paginator,
@@ -39,16 +40,18 @@ class UserController extends AbstractController
         ]);
     }
     /*********************************************************************** */
+
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
     /**
      * This controller allow us to edit user's profile
      *
      * @param UserRepository $repository
      * @param Request $request
      * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
      * @param [type] $id
      * @return Response
      */
-    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET', 'POST'])]
     public function edit(
         UserRepository $repository,
         Request $request,
@@ -59,39 +62,94 @@ class UserController extends AbstractController
 
         $user = $repository->findOneBy(["id" => $id]);
 
-        // if (!$this->getUser()) {
-        //     return $this->redirectToRoute('security.login');
-        // }
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('security.login');
+        }
 
-        // if ($this->getUser() !== $user) {
-        //     return $this->redirectToRoute('products');
-        // }
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('security.login');
+        }
 
 
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($hasher->isPasswordValid($user, $form->getData()->getPlainPassword()));
-            $user = $form->getData();
-            $manager->persist($user);
-            $manager->flush();
+            if ($hasher->isPasswordValid($user, $form->getData()->getPlainPassword())) {
+                $user = $form->getData();
+                $manager->persist($user);
+                $manager->flush();
 
-            $this->addFlash(
-                'success',
-                'Les informations de votre compte ont bien été modifiées !'
-            );
+                $this->addFlash(
+                    'success',
+                    'Les informations de votre compte ont bien été modifiées !'
+                );
 
-            return $this->redirectToRoute('products');
-        } else {
-            $this->addFlash(
-                'warning',
-                'Le mot de passe renseigné est incorrect .'
-            );
+                return $this->redirectToRoute('products');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect .'
+                );
+            }
         }
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    /************************************************************************************ */
+    #[Route('/utilisateur/edition-mot-de-passe/{id}', 'user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(
+        UserRepository $repository,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher,
+        $id
+    ): Response {
+        $user = $repository->findOneBy(["id" => $id]);
+
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('security.login');
+        }
+
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('security.login');
+        }
+
+
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setPassword(
+                    $hasher->hashpassword(
+                        $user,
+                        $form->getData()['newPassword']
+                    )
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a bien été modifié !'
+                );
+
+                return $this->redirectToRoute('products');
+            } else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect .'
+                );
+            }
+        }
+
+        return $this->render('pages/user/edit_password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
